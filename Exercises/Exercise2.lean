d3dbfeedbda57ac04b7e153c0c0409b7b88949bb
thisis-Shitanshu <shitanshu.19p@gmail.com>
2024-10-31 15:10:11 +0530

    Solution: Exercise 2

--- Exercises/Exercise2.lean
+++ Exercises/Exercise2.lean
@@ -1,4 +1,5 @@
 import Mathlib
+import Mathlib.Data.Nat.Choose.Basic
 
 open Nat
 
@@ -7,3 +8,15 @@ open Nat
   (i) binom n 0 = binom n n = 1
   (ii) binom n k = binom n (n - k)
 -/
+
+theorem binomial_properties (n k : ℕ) (h : k ≤ n) :
+  (choose n 0 = 1) ∧ (choose n n = 1) ∧ (choose n k = choose n (n - k)) := by
+  constructor
+  · -- Prove choose n 0 = 1
+    exact choose_zero_right n
+  constructor
+  · -- Prove choose n n = 1
+    exact choose_self n
+  · -- Prove choose n k = choose n (n - k)
+    rw [eq_comm]
+    exact @choose_symm n k h
